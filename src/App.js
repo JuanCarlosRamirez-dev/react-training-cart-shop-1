ab27dc96c106d728ace1d4282b2e97c2ca06cac1
JuanCarlosRamirez <58491643+JuanCarlosRamirez-dev@users.noreply.github.com>
2021-01-06 20:21:42 -0600

    header and main page format

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
-import './App.css';
+import React, { Component } from 'react';
+import logo from './assets/globant-shops.svg'
+import Products from './components/Products';
+import products from './api/mock-products';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      products: products
+    }
+  }
+
+  render() {
+    return (
+      <div className="grid-container">
+        <header>
+          <img src={logo} alt="Globant shops" />
+          <a href="/">React Shopping cart</a>
+        </header>
+        <main>
+          <div className="content">
+            <div className="filters">
+              FILTERS
+            </div>
+            <div className="products-sec">
+              <div className="our-products">Our Products:</div>
+              <Products products={this.state.products} />
+            </div>
+
+          </div>
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Products.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+
+export default class Products extends Component {
+
+
+    render() {
+        return (
+            <div>
+                <ul className="products">
+                    {this.props.products.map(product => (
+                        <li key={product._id}>
+                            <div className="product">
+                                <img src={/* product.image */"./coffe.jpg"} alt={product.name} />
+                                <div className="product-info">
+                                    <p>
+                                        {product.name}
+                                    </p>
+                                    <p>Stars and comments</p>
+                                    <div className="product-price">{"$" + product.price}</div>
+                                </div>
+                                <div className="buttons">
+                                    <button className="button blue">See details</button>
+                                    <button className="button green">Add to cart</button>
+                                </div>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        )
+    }
+}
