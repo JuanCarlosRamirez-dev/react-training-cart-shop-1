44d630593e6da6da9a0bac91001c7331d2d952ef
JuanCarlosRamirez <58491643+JuanCarlosRamirez-dev@users.noreply.github.com>
2021-01-07 21:40:41 -0600

    filters almost functional

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,32 +1,47 @@
+
 import React, { Component } from 'react'
 
-export default class Filter extends Component {
+export default class Filters extends Component {
+
     render() {
         return (
             <div className="filter">
                 <div>
                     <h3>Filters:</h3>
-                    <input
+
+
+
+                    {/*    <input
+                        onChange={this.props.filterProducts}
                         id="checkbox1"
                         type="checkbox"
                         name="filter"
-                        value="Basics"></input>
-                    <label htmlFor="checkbox1">Basics</label>
+                        value={this.props.basics}
+                    />
+                    <label htmlFor="checkbox1">Basics</label> */}
+
+                    <select value={this.props.basics} onChange={this.props.filterProducts} >
+                        <option value="NO BASICS">NO BASICS</option>
+                        <option value="BASICS">BASICS</option>
+                    </select>
 
                     <h3>Prices:</h3>
                     <input
+
                         id="radio1"
                         type="radio"
                         name="filter"
                         value="Radio1"></input>
                     <label htmlFor="radio1">$1 - $50</label> <br />
                     <input
+
                         id="radio2"
                         type="radio"
                         name="filter"
                         value="Radio2"></input>
                     <label htmlFor="radio2">$51 - $100</label> <br />
                     <input
+
                         id="radio3"
                         type="radio"
                         name="filter"
@@ -34,13 +49,13 @@ export default class Filter extends Component {
                     <label htmlFor="radio3">$101 - $200</label>
 
                     <h3>Sort by:</h3>
-                    <select id="sortby">
+                    <select value={this.props.sort} onChange={this.props.sortProducts} >
                         <option value="Relevant">Relevant</option>
                         <option value="Higher price">Higher price</option>
                         <option value="Lower price">Lower price</option>
                     </select>
                 </div>
             </div>
-        )
+        );
     }
 }
